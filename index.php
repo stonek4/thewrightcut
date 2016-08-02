c4884d3b2e8f0ce3d18718243ab30b1202d0705a
TylerLeite <tyler.leite@yandex.com>
2016-08-02 13:25:13 -0400

    Made marginally less terrible

--- index.php
+++ index.php
@@ -37,114 +37,111 @@
 
 <html>
 <head>
-    <title>The Wright Cut</title>
-    <link rel="stylesheet" href="index.css">
-    <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
-
+  <title>The Wright Cut</title>
+  <link rel="stylesheet" href="index.css">
+  <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
 </head>
 
 <body>
   <p class="h1">The Wright Cut | <a href="#employment">[Employment]</a> <a href="#upcoming">[Upcoming Events]</a> <a href="#contact">[Contact]</a></p>
 
   <div id="contact" class="container">
+      <p class="h2">Welcome</p>
+      <p class="h3">To The Wright Cut</p>
+      <p class="h4" id="contactString">info@twcbarbershop.com</p>
 
-        <p class="h2">Welcome</p>
-        <p class="h3">To The Wright Cut</p>
+      <center>
+        <a href="https://www.facebook.com/twcbarbershop/"><object type="image/svg+xml" data="img/facebook.svg" class="icon"></object></a>
+        <a href="https://www.instagram.com/TWCBS/"><object type="image/svg+xml" data="img/instagram.svg" class="icon"></object></a>
+        <a href="https://www.twitter.com/thewrightcutbs/"><object type="image/svg+xml" data="img/twitter.svg" class="icon"></object></a>
+        <a href="https://www.snapchat.com/add/TWCBS/"><object type="image/svg+xml" data="img/snapchat.svg" class="icon"></object></a>
+        <a id="ps"><object type="image/svg+xml" data="img/periscope.svg" class="icon"></object></a>
+        <a href="https://www.pinterest.com/TWCBarberShop/"><object type="image/svg+xml" data="img/pinterest.svg" class="icon"></object></a>
+        <a href="https://twcbarbershop.tumblr.com/"><object type="image/svg+xml" data="img/tumblr.svg" class="icon"></object></a>
+      </center>
 
-        <p class="h4" id="contactString">info@twcbarbershop.com</p>
+      <a class="contact" href="https://www.google.com/maps/place/55+Rockafeller+Rd+%2320,+Piscataway+Township,+NJ+08854/@40.5227071,-74.440489,17.75z/data=!4m5!3m4!1s0x89c3c797eb6e2dc7:0x37ce0a2fd78e44ce!8m2!3d40.522839!4d-74.4396809">
+        <p class="h4">55 Rockefeller Road Unit #20</p>
+        <p class="h4">Piscataway, NJ</p>
+      </a>
 
-        <center>
-          <a href="https://www.facebook.com/twcbarbershop/"><object type="image/svg+xml" data="img/facebook.svg" class="icon"></object></a>
-          <a href="https://www.instagram.com/TWCBS/"><object type="image/svg+xml" data="img/instagram.svg" class="icon"></object></a>
-          <a href="https://www.twitter.com/thewrightcutbs/"><object type="image/svg+xml" data="img/twitter.svg" class="icon"></object></a>
-          <a href="https://www.snapchat.com/add/TWCBS/"><object type="image/svg+xml" data="img/snapchat.svg" class="icon"></object></a>
-          <a id="ps"><object type="image/svg+xml" data="img/periscope.svg" class="icon"></object></a>
-          <a href="https://www.pinterest.com/TWCBarberShop/"><object type="image/svg+xml" data="img/pinterest.svg" class="icon"></object></a>
-          <a href="https://twcbarbershop.tumblr.com/"><object type="image/svg+xml" data="img/tumblr.svg" class="icon"></object></a>
-        </center>
+      <span id="upcoming" class="space"></span>
 
-        <a class="contact" href="https://www.google.com/maps/place/55+Rockafeller+Rd+%2320,+Piscataway+Township,+NJ+08854/@40.5227071,-74.440489,17.75z/data=!4m5!3m4!1s0x89c3c797eb6e2dc7:0x37ce0a2fd78e44ce!8m2!3d40.522839!4d-74.4396809">
-          <p class="h4">55 Rockefeller Road Unit #20</p>
-          <p class="h4">Piscataway, NJ</p>
-        </a>
+      <img class="flyer" src="./img/TWCF.jpg"/>
 
-        <span class="space"></span>
-        <span id="upcoming" class="space"></span>
+      <span id="employment" class="space"></span>
+      <p class="h3">Join Our Team</p>
+      <p class="p1">A career with The Wright Cut features a one-of-a-kind environment great pay & incentive, and guaranteed clientele.</p>
+      <p class="p1">We're interviewing motivated, positive, reliable, upbeat, Master Barbers / Managers and licensed Barber / Stylists to help grow and be one of our valued team members. You should be outgoing and career-oriented, as well as have a current license and passion for doing men's and boys' haircuts</p>
+      <p class="p1">This interview should take a few minutes to complete. Enter or select a response for each question below</p>
 
-        <img class="flyer" src="./img/TWCF.jpg"/>
+      <span class="space"></span>
 
-        <span class="space"></span>
-        <span id="employment" class="space"></span>
-        <p class="h3">Join Our Team</p>
-        <p class="p1">A career with The Wright Cut features a one-of-a-kind environment great pay & incentive, and guaranteed clientele.</p>
-        <p class="p1">We're interviewing motivated, positive, reliable, upbeat, Master Barbers / Managers and licensed Barber / Stylists to help grow and be one of our valued team members. You should be outgoing and career-oriented, as well as have a current license and passion for doing men's and boys' haircuts</p>
-        <p class="p1">This interview should take a few minutes to complete. Enter or select a response for each question below</p>
+      <?php
+          if (empty($_POST)) {
+      ?>
 
-        <span class="space"></span>
+      <center>
+        <form method="post" action="" class="interview">
+          <div class="left-50">
+            <label class="noselect" for="email">Email Address</label><br>
+            <input type="text" name="email" placeholder="email@example.com"
+                       onfocus="(this.placeholder='')" onblur="(this.placeholder='email@example.com')"/><br>
 
-        <?php
-            if (empty($_POST)) {
-        ?>
+            <label class="noselect" for="name">Full Name</label><br>
+            <input type="text" name="name" placeholder="Tim Wright"
+                       onfocus="(this.placeholder='')" onblur="(this.placeholder='Tim Wright')"/><br>
 
-        <center>
-          <form method="post" action="" class="interview"><div class="left-50">
+            <label class="noselect" for="phone">Phone Number</label><br>
+            <input type="text" name="phone" placeholder="(000) 111 - 2222"
+                       onfocus="(this.placeholder='')" onblur="(this.placeholder='(000) 111 - 2222')"/><br>
 
-                   <label class="noselect" for="email">Email Address</label><br>
-                   <input type="text" name="email" placeholder="email@example.com"
-                               onfocus="(this.placeholder='')" onblur="(this.placeholder='email@example.com')"/><br>
+            <label  class="noselect" for="instagram">Instagram Handle</label><br>
+            <input type="text" name="instagram" placeholder="@TheWrightCut"
+                       onfocus="(this.placeholder='')" onblur="(this.placeholder='@TheWrightCut')"/><br>
+         </div>
+         <div class="right-50">
+           <label class="noselect" for="state">State</label><br>
+           <select name="state">
+               <option selected value="New Jersey">New Jersey</option>
+               <option value="Pennsylvania">Pennsylvania</option>
+               <option value="New York">New York</option>
+           </select><br>
 
-                   <label class="noselect" for="name">Full Name</label><br>
-                   <input type="text" name="name" placeholder="Tim Wright"
-                               onfocus="(this.placeholder='')" onblur="(this.placeholder='Tim Wright')"/><br>
+           <label class="noselect" for="city">City</label><br>
+           <input type="text" name="city" placeholder="Springfield"
+                       onfocus="(this.placeholder='')" onblur="(this.placeholder='Springfield')"/><br>
 
-                   <label class="noselect" for="phone">Phone Number</label><br>
-                   <input type="text" name="phone" placeholder="(000) 111 - 2222"
-                               onfocus="(this.placeholder='')" onblur="(this.placeholder='(000) 111 - 2222')"/><br>
+           <label class="noselect" for="zip">Zip Code</label><br>
+           <input type="text" name="zip" placeholder="12340"
+                       onfocus="(this.placeholder='')" onblur="(this.placeholder='12340')"/><br>
 
-                   <label  class="noselect" for="instagram">Instagram Handle</label><br>
-                   <input type="text" name="instagram" placeholder="@TheWrightCut"
-                               onfocus="(this.placeholder='')" onblur="(this.placeholder='@TheWrightCut')"/><br>
-           </div><div class="right-50">
-                   <label class="noselect" for="state">State</label><br>
-                   <select name="state">
-                       <option selected value="New Jersey">New Jersey</option>
-                       <option value="Pennsylvania">Pennsylvania</option>
-                       <option value="New York">New York</option>
-                   </select><br>
+           <label class="noselect" for="refer">How did you hear about us?</label><br>
+           <select name="refer">
+               <option selected value="a representative">The Wright Cut representitve</option>
+               <option value="a walk-in">Walk-In</option>
+               <option value="an on-campus advertisement">On-campus Advertisement</option>
+               <option value="an off-campus advertisement">Off-campus Advertisement</option>
+           </select><br>
+         </div>
 
-                   <label class="noselect" for="city">City</label><br>
-                   <input type="text" name="city" placeholder="Springfield"
-                               onfocus="(this.placeholder='')" onblur="(this.placeholder='Springfield')"/><br>
+         <input type="submit" class="submit" value="Apply now">
+       </form>
+      </center>
 
-                   <label class="noselect" for="zip">Zip Code</label><br>
-                   <input type="text" name="zip" placeholder="12340"
-                               onfocus="(this.placeholder='')" onblur="(this.placeholder='12340')"/><br>
+      <?php
+          } else {
+      ?>
 
-                   <label class="noselect" for="refer">How did you hear about us?</label><br>
-                   <select name="refer">
-                       <option selected value="a representative">The Wright Cut representitve</option>
-                       <option value="a walk-in">Walk-In</option>
-                       <option value="an on-campus advertisement">On-campus Advertisement</option>
-                       <option value="an off-campus advertisement">Off-campus Advertisement</option>
-                   </select><br>
-             </div>
-             <input type="submit" class="submit" value="Apply now">
-           </form>
-        </center>
+      <p class="thanks">Your form has been submitted! We'll get back to you shortly.</p>
 
-        <?php
-            } else {
-        ?>
+      <?php
+          }
+      ?>
 
-        <p class="thanks">Your form has been submitted! We'll get back to you shortly.</p>
+      <p class="footer">Website by Supa TK Web Design</p>
 
-        <?php
-            }
-        ?>
-
-        <p class="footer">Website by Supa TK Web Design</p>
-
-    </div>
+  </div>
 </body>
 
 </html>
